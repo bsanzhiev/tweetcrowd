204d70d80b19d81d9143fe940d02dfbb8248b42d
bator <bsanzhiev@outlook.com>
2023-10-30 23:05:35 +0800

    frontend is done

--- app/page.tsx
+++ app/page.tsx
@@ -2,47 +2,94 @@
 
 import { useState } from "react";
 import {
-	Box,
-	Button,
-	Container,
-	Image,
-	Input,
-	Text,
-	TextInput,
-	Title,
+  Badge,
+  Box,
+  Button,
+  Card,
+  Container,
+  Group,
+  Image,
+  Input,
+  Text,
+  TextInput,
+  Title,
 } from "@mantine/core";
 import NextImage from "next/image";
 import elonJpg from "../public/elon.jpg";
 
 export default function HomePage() {
-	const [isWalletConnected, setIsWalletConnected] = useState(true);
+  const [isWalletConnected, setIsWalletConnected] = useState(true);
 
-	return (
-		<Container size={"md"} ta={"center"} my={24}>
-			<header>
-				<Image component={NextImage} src={elonJpg} alt="Asshole" />
-				<Title>Fuck Elon!</Title>
-			</header>
+  const connectedAccount = "0x1501010100110010101x0000010101";
 
-			{isWalletConnected ? (
-				<>
-					<Text>Connected Account:</Text>
-					<Box>
-						<TextInput label="WEI" />
-						<Button onClick={() => console.log("click")}>Donate</Button>
-					</Box>
-					<Box>
-						<Button onClick={() => console.log("click2")}>
-							See Total Contribution Amount
-						</Button>
-					</Box>
-				</>
-			) : (
-				<Container>
-					<Button>Connect Wallet</Button>
-				</Container>
-			)}
-			<Text>Response Message</Text>
-		</Container>
-	);
+  return (
+    <>
+      <Container size={"sm"} ta={"center"}>
+        <header>
+          <Container p={0} mb={36}>
+            <Image
+              component={NextImage}
+              height={300}
+              src={elonJpg}
+              alt="Asshole"
+            />
+            <Title mt={12}>Fuck Elon!</Title>
+          </Container>
+        </header>
+
+        {isWalletConnected ? (
+          <Container
+            style={{ display: "flex", flexDirection: "column" }}
+            size={"xs"}
+          >
+            <Card
+              padding="lg"
+              withBorder
+              mb={36}
+              style={{ justifyContent: "space-between" }}
+            >
+              <Group align="center" justify="space-between">
+                <Text>Connected Account:</Text>
+                <Badge variant="outline" color="blue" size="lg" radius="md">
+                  {connectedAccount}
+                </Badge>
+              </Group>
+            </Card>
+            <Card padding="lg" withBorder mb={36}>
+              <Group justify="space-between">
+                <TextInput
+                  style={{ width: "fit-content", maxWidth: "100%" }}
+                  rightSection={<Text>WEI</Text>}
+                  defaultValue={10000000000000000}
+                />
+                <Button onClick={() => console.log("click")}>Donate</Button>
+              </Group>
+            </Card>
+            <Box>
+              <Button onClick={() => console.log("click1")}>
+                See Total Contribution Amount
+              </Button>
+            </Box>
+            <Box>
+              <Button mt={24} onClick={() => console.log("click2")}>
+                Request Refund
+              </Button>
+            </Box>
+            <Box>
+              <Button mt={24} color="red" onClick={() => console.log("click3")}>
+                Reset Connection
+              </Button>
+            </Box>
+          </Container>
+        ) : (
+          <Container>
+            <Button>Connect Wallet</Button>
+          </Container>
+        )}
+        <Container mt={16}>
+          <Text>Response Message</Text>
+        </Container>
+      </Container>
+    </>
+  );
 }
